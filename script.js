871079f05957035032185e44908efe1817f63359
Ramc26 <rakeshchowdary885@gmail.com>
2024-03-09 15:49:40 -0600

    added email functional using EmailJS

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "0BTonjp4iBF33pc3Q",
+    });
+})();
 
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+        .then(() => {
+                console.log('SUCCESS!');
+            }, (error) => {
+                console.log('FAILED...', error);
+            });
+    });
+}
 
 // $(document).ready(function(){
 //   $(window).scroll(function(){
@@ -20,6 +38,8 @@ $(document).ready(function() {
     });
   });
 });
+
+
 
 
 //  let slideIndex = 0;
